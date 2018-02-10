cfb00b29fabbba5314e4d5f6aedacf0627ca76f3
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-02-10 10:32:09 +0530

    Update a27.c

--- a27.c
+++ a27.c
@@ -12,13 +12,13 @@ void main()
     }
     for(i=0;i<n-1;i++)
     {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[i]>a[j])
+       for(j=i+1;j<n;j++)
+       {
+          if(a[i]>a[j])
             {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+              temp=a[i];
+              a[i]=a[j];
+              a[j]=temp;
             }
         }
     }
