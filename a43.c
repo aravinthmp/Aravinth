66ba334cec1720cc982397b01bce7116c7e897b1
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-02-12 13:42:35 +0530

    Create a43.c

---
+++ a43.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main(void) {
+ int k,i;
+ char s;
+ printf("enter the k ");
+ scanf("%d",&k);
+ printf("enter the string");
+ scanf("%s",s);
+ for(i=0;i<k;i++)
+ {
+ 	printf("%s\n",s);
+ }
+	return 0;
+ } 
