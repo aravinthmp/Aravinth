2fe27d7855597feacd0a016b08c512835755b0ea
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-02-12 13:06:13 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include<conio.h>
+void main()
+{
+   char a[50];
+   int count = 0, i;
+ 
+   printf("enter the string:\n");
+   scanf("%[^\n]s", a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if (a[i] == ' ')
+            count++;    
+    }
+    printf("%d\n", count);
+    getch();
+}
