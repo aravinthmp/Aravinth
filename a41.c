f3ec7d76b05112bea5f4e1d459e8978453df391c
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-02-12 13:29:29 +0530

    Create a41.c

---
+++ a41.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   int i = 65;
+   printf(" value of i=%d before swapping", i, k);
+   i = i ^ k;
+   printf("value of i=%d after swapping", i, k);
+   return 0;
+}
