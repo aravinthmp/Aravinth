26bd666861c1506c6dd0aaa4e58d5c59aa3a5728
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-02-19 03:07:50 -0500

    Create a50.c

---
+++ a50.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+	 long int a,temp;
+	 int result;
+	 scanf("%d",&a);
+	 temp=a;
+	 while(temp!=0)
+	 {
+		temp=temp/10;
+		result++;
+	 }
+	 printf("%d",result);
+	return 0;
