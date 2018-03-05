5fe14437f3c5597d8032e6ff6a202186b4b390c8
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-03-05 13:23:31 +0530

    Create Amp12.c

---
+++ Amp12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char a[100], b[100];
+   gets(a);
+   strcpy(b,a);
+   strrev(b);
+   if (strcmp(a,b) == 0)
+      printf("Yes\n");
+   else
+      printf("No\n");
+   return 0;
+}
