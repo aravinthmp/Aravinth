8f8428b54e8f324f05626e426a37da178c478411
aravinthmp <35992930+aravinthmp@users.noreply.github.com>
2018-03-23 16:10:18 +0530

    Update Amp46.c

--- Amp46.c
+++ Amp46.c
@@ -4,11 +4,19 @@
 int main(void) 
 {
 	int n,k,i,result=1;
+	
 	scanf("%d %d",&n,&k);
+	
 	for(i=1;i<=k;i++)
+		
 	{
-		result=result*n;	
+		
+		result=result*n;
+		
 	}
+	
 	printf("%d",result);
+	
 	return 0;
+	
 }
